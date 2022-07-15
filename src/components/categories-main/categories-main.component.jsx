94babb44bc6172522b5fac9fcb51f1beed369bea
sal998 <92775669+sal998@users.noreply.github.com>
2022-07-15 14:46:31 +0530

    Updating categgory and categories

---
+++ src/components/categories-main/categories-main.component.jsx
@@ -0,0 +1,14 @@
+import "./categories-main.styles.scss";
+import CategoryItem from "../category-item/category-item.component";
+
+const CategoriesMain = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default CategoriesMain;
